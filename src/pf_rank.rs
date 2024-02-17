bf4040b5bbe21e0be774c8d8b2e32d91693deab5
hansel <hansel@openb.net>
2024-02-17 11:04:41 +0800

    damnit analyzer isnt on

--- src/pf_rank.rs
+++ src/pf_rank.rs
@@ -87,7 +87,7 @@ fn update_total_for_straightness(total: f32, value: u8, diff: u8) -> f32 {
         1 => value as f32 / 1.5,
         2 => value as f32 / 2.5,
         3 => value as f32 / 3.5,
-        4 => value as f32 / 5,
+        4 => value as f32 / 5.0,
         _ => 0.0,
     };
     total + multiplier
